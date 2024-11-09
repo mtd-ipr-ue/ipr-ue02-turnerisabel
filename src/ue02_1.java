7c9826e24c0fd23c49c61e1994bf6ce43ad89091
turnerisabel <turner.isabel03@gmail.com>
2024-11-09 13:32:40 +0100

    implemented ue02_1

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Geben Sie die zweite Ganzzahl ein: ");
+        int firstNumber = myScanner.nextInt();
+
+        System.out.println("Geben Sie die zweite Ganzzahl ein: ");
+        int secondNumber = myScanner.nextInt();
+
+        System.out.println("Geben Sie die dritte Ganzzahl ein: ");
+        int thirdNumber = myScanner.nextInt();
+
+        myScanner.close();
+
+        int maxNum = 0;
+
+        if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+            maxNum = firstNumber;
+        } else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
+            maxNum = secondNumber;
+        } else {
+            maxNum = thirdNumber;
+        }
+        System.out.println("Das Maximum der eingegebenen Werte ist: " + maxNum);
     }
 }
