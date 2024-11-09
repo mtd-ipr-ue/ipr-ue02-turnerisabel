103e1843fba7fc14bd45448c5480258674f99630
turnerisabel <turner.isabel03@gmail.com>
2024-11-09 14:54:01 +0100

    implemented ue02_3

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Geben Sie die x-Koordinate ein: ");
+        double x = myScanner.nextDouble();
+
+        System.out.println("Geben Sie die y-Koordinate ein: ");
+        double y = myScanner.nextDouble();
+
+        myScanner.close();
+
+        if ((x > 0 && y > 0 && x <=4 && y <=4) && x+y > 4) {
+            System.out.println("Liegt im grünen Bereich");
+        }else{
+            System.out.println("Liegt außerhalb des grünen Bereichs");
+        }
     }
 }
