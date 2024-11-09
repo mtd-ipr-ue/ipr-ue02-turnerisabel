f1ca7c92a630e35676315e9e823604ed935be07f
turnerisabel <turner.isabel03@gmail.com>
2024-11-09 14:28:22 +0100

    implemented ue02_2

--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,7 +1,44 @@
-import libraries.Out;
+import java.util.Scanner;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Geben Sie eine Jahreszahl ein: ");
+        int year = myScanner.nextInt();
+
+        System.out.println("Geben Sie ein Monat zwischen 1-12 ein: ");
+        int month = myScanner.nextInt();
+
+        myScanner.close();
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12: 
+                System.out.println("Das Monat hat 31 Tage.");
+            break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+              System.out.println("Das Monat hat 30 Tage");
+            break;
+            case 2: 
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println("Das eingegebene Jahr ist ein Schaltjahr und hat 29 Tage.");
+            }else {
+                System.out.println("Das eingegbene Jahr ist kein Schaltjahr und hat 28 Tage.");
+            }
+            default: 
+            if (month < 1 || month > 12) {
+                System.out.println("Der eingegebene Wert liegt nicht zwischen 1 und 12!");
+            }
+           break;     
+        }
     }
 }
